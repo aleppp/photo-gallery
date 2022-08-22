ac3a2f5f0fdd8eeba723a339efe6ba6c37f6edb7
Alif Muqri Hazmi <alifmuqri.hazmi@petronas.com>
2022-08-23 01:40:08 +0800

    add feature to save photo to filesystem

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -15,16 +15,51 @@ export class PhotoService {
   public async addNewToGallery(){
     // Take a photo
     const capturedPhoto = await Camera.getPhoto({
-      resultType: CameraResultType.Uri,
-      source: CameraSource.Camera,
-      quality: 100
+      resultType: CameraResultType.Uri, // file-based data; provides best performance
+      source: CameraSource.Camera, //automatically take a new photo with the camera
+      quality: 100 //highest quality
     });
 
-    this.photos.unshift({
-      filepath: 'soon...',
-      webviewPath: capturedPhoto.webPath
-    });
+    //save picture and add it to photo collection
+    const savedImageFile = await this.savePicture(capturedPhoto);
+    this.photos.unshift(savedImageFile);
   }
+
+  private async savePicture(photo: Photo) {
+    //convert photo to base64 format, required by filesystem API to save
+    const base64Data = await this.readAsBase64(photo);
+
+    //Write the file to the data directory
+    const fileName = new Date().getTime() + '.jpeg';
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data
+    });
+
+    //Use webpath to display the new image instead of base64 since it's loaded into memory
+    return {
+      filepath: fileName,
+      webviewPath: photo.webPath
+    };
+  }
+
+  private async readAsBase64(photo: Photo) {
+    //Fetch the photo, read as blob, then convert to base64 format
+    const response = await fetch(photo.webPath!);
+    const blob = await response.blob();
+
+    return await this.convertBlobToBase64(blob) as string;
+  }
+
+  private convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onerror = reject;
+    reader.onload = () => {
+      resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+  });
 }
 
 
